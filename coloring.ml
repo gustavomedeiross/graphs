7534feacb8f7c9eca52bb8dcc40ae068a6c2fe37
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2021-11-17 21:31:36 -0300

    Add unit tests with alcotest

--- coloring.ml
+++ coloring.ml
@@ -1,19 +1,24 @@
 open Core
 
 type 'a vertex = 'a
-  [@@deriving show]
+  [@@deriving show, eq]
 
 type 'a edge = 'a * 'a
-  [@@deriving show]
+  [@@deriving show, eq]
 
 type 'a graph = Graph of 'a vertex list * 'a edge list
-  [@@deriving show]
+  [@@deriving show, eq]
 
 type 'a adjacency = Adjacency of ('a * 'a list) list
-  [@@deriving show]
+  [@@deriving show, eq]
 
 type color = int
-  [@@deriving show]
+  [@@deriving show, eq]
+
+type state = (int vertex * color) list
+  [@@deriving show, eq]
+
+let state_testable = Alcotest.testable pp_state equal_state
 
 let adj_vertices vertex edges =
   List.filter edges ~f:(fun (x, y) -> (Poly.equal vertex x) || (Poly.equal y vertex))
@@ -42,9 +47,6 @@ let get_vertex_color state vertex =
   List.find state ~f:(fun (v, _) -> Poly.equal vertex v)
   |> Option.map ~f:(fun (_, color) -> color)
   |> Option.value_exn
-
-type state = (int vertex * color) list
-  [@@deriving show]
 
 let rec select_color' state nghbs color =
   let nghbs_colors = List.map nghbs ~f:(fun v -> get_vertex_color state v) in
@@ -76,20 +78,48 @@ let coloring (graph : 'a graph) : ('a vertex * color) list =
   let initial_state = List.map adj ~f:(fun v -> (fst v, -1)) in
   color initial_state (Adjacency adj)
 
-let () =
-  let sample_graph = Graph(
-      [1; 2; 3; 4; 5; 6; 7; 8],
-      [
-        (1, 2); (1, 3); (1, 4); (1, 7);
-        (2, 1); (2, 5); (2, 6);
-        (3, 1); (3, 7);
-        (4, 1); (4, 7);
-        (5, 2); (5, 6); (5, 8);
-        (6, 2); (6, 5); (6, 8);
-        (7, 1); (7, 3); (7, 4);
-        (8, 5); (8, 6); (8, 7);
-      ]
-    ) in
-  let result = coloring sample_graph in
-  Out_channel.print_endline (show_state result)
+let test_graph_1 () =
+  let g = Graph(
+    [1; 2; 3; 4; 5; 6; 7; 8],
+    [
+      (1, 2); (1, 3); (1, 4); (1, 7);
+      (2, 1); (2, 5); (2, 6);
+      (3, 1); (3, 7);
+      (4, 1); (4, 7);
+      (5, 2); (5, 6); (5, 8);
+      (6, 2); (6, 5); (6, 8);
+      (7, 1); (7, 3); (7, 4);
+      (8, 5); (8, 6); (8, 7);
+    ]
+  ) in
+  Alcotest.(check state_testable) 
+    "valid coloring" 
+    [(1, 1); (2, 2); (3, 2); (4, 2); (5, 1); (6, 3); (7, 3); (8, 2)] 
+    (coloring g)
 
+let test_graph_2 () =
+  let g = Graph(
+    [1; 2; 3; 4; 5; 6;],
+    [
+      (1, 2); (1, 4); (1, 6);
+      (2, 1); (2, 3); (2, 5);
+      (3, 2); (3, 4); (3, 6);
+      (4, 1); (4, 3); (4, 5);
+      (5, 2); (5, 4); (5, 6);
+      (6, 1); (6, 3); (6, 5);
+    ]
+  ) in
+  Alcotest.(check state_testable) 
+    "valid coloring" 
+    [(1, 1); (2, 2); (3, 1); (4, 2); (5, 1); (6, 2)]
+    (coloring g)
+
+
+let () =
+  let open Alcotest in
+  run "Graph Coloring" [
+    "Graph coloring", [
+      test_case "Graph #1" `Quick test_graph_1;
+      test_case "Graph #2" `Quick test_graph_2;
+    ]
+  ]
